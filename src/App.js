fd04d9cfa5a938d099bb9c5e17aed6f19af3e957
BaoPham26111998 <s3695363@rmit.edu.vn>
2021-07-10 00:06:02 +0700

    react installed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cai du ma may
         </a>
       </header>
     </div>
