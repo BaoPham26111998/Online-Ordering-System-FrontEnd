cbfa14de0e11ac3ed311b876e92e9f000199d544
BaoPham26111998 <s3695363@rmit.edu.vn>
2021-07-14 18:04:10 +0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cai du ma may
-        </a>
+     <head>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <title>Need A Team Name Gaming</title>
+  </head>
+  <body>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">Need A Team Name Gaming </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      
+      <main>
+        <div>
+        
+          <div className="row center">
+            {
+              data.products.map(product => (
+                <div  className="card">
+              <a href="product.html">
+               
+                <img className="medium" src="https://upload.wikimedia.org/wikipedia/vi/5/52/Assassin%27s_Creed_III.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2 className= "product-name">Assasin's creed III</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">$120</div>
+              </div>
+            </div>
+              ))
+            }
+            
+            
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  </body>
     </div>
   );
 }
