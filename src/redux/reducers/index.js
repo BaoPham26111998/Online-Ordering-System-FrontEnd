fcdf14706eceeccc5e6602b49cd284e009ae81ad
tinskin1 <tinskin1@gmail.com>
2021-07-18 10:26:56 +0700

    new react structure

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,6 @@
 import { combineReducers } from "redux";
-import listEventReducer from "containers/HomeTemplate/ListEventPage/modules/reducer";
-import detailEventReducer from "containers/HomeTemplate/DetailEventPage/modules/reducer";
-// import authReducer from "containers/AdminTemplate/AuthPage/modules/reducer";
-// import addUserReducer from "containers/AdminTemplate/AddUserPage/modules/reducer";
 
 const rootReducer = combineReducers({
-    listEventReducer,
-    detailEventReducer,
-    // authReducer,
-    // addUserReducer,
 });
 
 export default rootReducer;
