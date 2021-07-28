9788d16a64e0d65780b0cb3ea30ffa1874d12e25
BaoPham26111998 <s3695363@rmit.edu.vn>
2021-07-28 21:17:47 +0700

    add search function

--- src/containers/HomeTemplate/SearchPage/index.js
+++ src/containers/HomeTemplate/SearchPage/index.js
@@ -1,79 +1,271 @@
 import './search.css';
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  Card,
+  Table,
+  Image,
+  ButtonGroup,
+  Button,
+  InputGroup,
+  FormControl,
+  Dropdown,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faList,
+  faEdit,
+  // faStepBackward,
+  // faFastBackward,
+  // faStepForward,
+  // faFastForward,
+  faSearch,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import ItemService from 'services/index.js';
+import { faShoppingCart } from '../../../../node_modules/@fortawesome/free-solid-svg-icons/index';
 
 
-export default function SearchPage() {
-    
+
+
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      search: "",
+      genre: "",
+      sortDir: "asc",
+    };
+  }
+
+
+
+  componentDidMount() {
+    this.findAllItems()
+  }
+
+  sortData = () => {
+    setTimeout(() => {
+      this.state.sortDir === "asc"
+        ? this.setState({ sortDir: "desc" })
+        : this.setState({ sortDir: "asc" });
+      this.findAllItems();
+    }, 500);
+  };
+
+  findAllItems() {
+    ItemService.getItems()
+      .then(res => {
+        console.log(res.data)
+        this.setState({ products: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  searchChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  };
+
+  gerneChange = (event) => {
+    this.setState({
+      [event.target.genreName]: event.target.value,
+    })
+    this.genreData()
+  };
+
+  cancelSearch = () => {
+    ItemService.getItems()
+      .then(res => {
+        console.log(res.data)
+        this.setState({ products: res.data });
+      })
+      .catch(err => console.log(err));
+  };
+
+  searchData = () => {
+    ItemService.getItemByName(this.state.search)
+      .then(res => {
+        console.log(this.state.search)
+        this.setState({ products: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // sortData = () => {
+  //   setTimeout(() => {
+  //     this.state.sortDir === "asc"
+  //       ? this.setState({ sortDir: "desc" })
+  //       : this.setState({ sortDir: "asc" });
+  //     this.findAllItems();
+  //   }, 500);
+  // };
+
+  genreData = () =>{
+    ItemService.getItemByGerne(this.state.genre)
+      .then(res => {
+        console.log(this.state.genre)
+        this.setState({ products: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  
+
+  render() {
+    const { products, search } = this.state;
+    var genre = [...new Set(products.map(product => product.genre))]
     return (
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="/">Need A Team Name Gaming </a>
-                </div>
-            </header>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">Need A Team Name Gaming </a>
+          </div>
+        </header>
 
-            <main>
-                <div className="GameFilter">
-                
-                    <div className ="GameFilterMain">
-                    
-                    <div class="list-game-filter-item">
-                        <label for="filter-name">Name</label>
-                        <form>   
-                            <input class="w3-input" type="text"></input>   
-                        </form>
-                        </div>
-                    
-                    
-                    <div class="list-game-filter-item">
-                        <label for="filter-category">Category</label>
-                        <select class="form-control">
-                            <option value="">All</option>
-                            <option value="1">Action</option>
-                            <option value="2">FPS</option>
-                            <option value="3">TPS</option>
-                            </select>
-                        </div>
+        <main>
 
-                        <div class="list-game-filter-item">
-                        <label for="filter-price">Price</label>
-                        <select class="form-control">
-                            <option value="">All</option>
-                            <option value="1">Ascending</option>
-                            <option value="2">Descending</option>
-                            </select>
-                        </div>
+          <Card className={"border border-dark bg-dark text-white"}>
+            <Card.Header>
+              <div style={{ float: "left" }}>
+                <FontAwesomeIcon icon={faList} /> Product List
+              </div>
+              <div style={{ float: "right" }}>
+                <InputGroup size="sm">
+                  
+                  <Dropdown as={ButtonGroup}>
+                    <Button variant="Secondary">Genre</Button>
 
-                        <div class="list-game-filter-item">
-                        <label for="filter-rating">Rating</label>
-                        <select  class="form-control">
-                            <option value="">All</option>
-                            <option value="1">Ascending</option>
-                            <option value="2">Descending</option>
-                            </select>
-                        </div>
+                    <Dropdown.Toggle split variant="Secondary" id="dropdown-split-basic" />
 
-                        <div class="list-game-filter-item">
-                        <label for="filter-review">Review</label>
-                        <select  class="form-control">
-                            <option value="">All</option>
-                            <option value="1">Ascending</option>
-                            <option value="2">Descending</option>
-                            </select>
-                        </div>
+                    <Dropdown.Menu>
+                    <Dropdown.Item 
+                    genreName="genre"
+                    value={genre}
+                    onClick={this.genreChange}
+                    >{genre}</Dropdown.Item>
+                      
+                      
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  
+                  <FormControl
+                    placeholder="Search"
+                    name="search"
+                    value={search}
+                    className={"info-border bg-dark text-white"}
+                    onChange={this.searchChange}
+                  />
+                  <InputGroup.Append>
+                    <Button
+                      size="sm"
+                      variant="outline-info"
+                      type="button"
+                      onClick={this.searchData}
+                    >
+                      <FontAwesomeIcon icon={faSearch} />
+                    </Button>
+                    <Button
+                      size="sm"
+                      variant="outline-danger"
+                      type="button"
+                      onClick={this.cancelSearch}
+                    >
+                      <FontAwesomeIcon icon={faTimes} />
+                    </Button>
+                  </InputGroup.Append>
+                </InputGroup>
+              </div>
+            </Card.Header>
+            <Card.Body>
+              <Table bordered hover striped variant="dark">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>In Stock</th>
+                    <th onClick={this.sortData}>
+                      Price{" "}
+                      <div
+                        className={
+                          this.state.sortDir === "asc"
+                            ? "arrow arrow-up"
+                            : "arrow arrow-down"
+                        }
+                      >
+                        {" "}
+                      </div>
+                    </th>
+                    <th>Genre</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.products.length === 0 ? (
+                    <tr align="center">
+                      <td colSpan="7">No products Available.</td>
+                    </tr>
+                  ) : (
+                    products.map((product) => (
+                      <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>
+                          <Image
+                            src={product.img}
+                            roundedCircle
+                            width="25"
+                            height="25"
+                          />{" "}
+                          {product.title}
+                        </td>
+                        <td>{product.inStock}</td>
+                        <td>{product.price}</td>
+                        <td>{product.genre}</td>
+                        <td>
+                          <ButtonGroup>
+                            <Link
 
-                        <div class="list-game-filter-item">
-                        <button className="apply-search"> APPLY</button>
-                        </div>
+                              to={"product/" + product.id}
+                              className="btn btn-sm btn-outline-primary"
+                            >
+                              <FontAwesomeIcon icon={faEdit} /><p>View</p>
+                            </Link>{" "}
+                            <Button
+                              size="sm"
+                              variant="outline-success"
+                              onClick={() => (product.id)}
+                            >
+                              <FontAwesomeIcon icon={faShoppingCart} />
+                              <p>Add to cart</p>
+                            </Button>
+                          </ButtonGroup>
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
 
+        </main>
 
-                    
-                    </div>
-                </div>
-            </main>
-
-            <footer className="row center">All right reserved</footer>
-        </div>
+        <footer className="row center">All right reserved</footer>
+      </div>
     )
+  }
+
 
 }
+
+const mapStateToProps = (state) => {
+  return {
+    productObject: state.product,
+  };
+};
+
+
+export default connect(mapStateToProps)(SearchPage);
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const Items_REST_API_URL = 'http://localhost:8080/items/';
+const Items_Name_URL = 'http://localhost:8080/items/title=';
+const Items_Genre_URL = 'http://localhost:8080/items/genre=';
 
 class ItemService {
 
@@ -8,7 +10,15 @@ class ItemService {
         return axios.get(Items_REST_API_URL);
     }
     getItemById(itemId){
-        return axios.get(Items_REST_API_URL +itemId );
+        return axios.get(Items_REST_API_URL + itemId );
+    }
+    getItemByName(gameName){
+        gameName.replace(" ","%20");
+        return axios.get(Items_Name_URL+gameName);
+    }
+    getItemByGerne(gameGerne){
+        gameGerne.replace(" ","%20");
+        return axios.get(Items_Genre_URL+gameGerne);
     }
 }
 
