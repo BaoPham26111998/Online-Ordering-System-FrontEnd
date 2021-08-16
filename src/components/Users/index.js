5005899a86679f4ea9ced7b8c34369156d1c1f49
tinskin1 <tinskin1@gmail.com>
2021-08-16 11:29:57 +0700

    role and locked

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -5,7 +5,7 @@ export default class User extends Component {
 
     render() {
         const { deleteUser, user } = this.props;
-        
+
 
         return (
             <>
@@ -13,7 +13,8 @@ export default class User extends Component {
                 <td >{user.username}</td>
                 <td >{user.name}</td>
                 <td >{user.email}</td>
-                <td>{user.password}</td>
+                <td>{user.appUserRole}</td>
+                <td>{(user.locked).toString()}</td>
                 <td><button className="btn btn-danger" onClick={deleteUser.bind(this, user.id)}><i className="fa fa-trash"></i></button></td>
             </>
         )
